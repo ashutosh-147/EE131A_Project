99f7e3a6a0a489f2220fce1146e0c7035a40da63
Ashutosh Shiledar <ashutosh@lnxsrv02.seas.ucla.edu>
2014-12-16 14:18:28 -0800

    added comments and better formatting to Qestion 1

--- Question1.m
+++ Question1.m
@@ -1,13 +1,16 @@
+%Create a binimial distribution with specified parameters
 binomialDist = makedist('Binomial','N',100,'p',0.2);
-x = random(binomialDist, 10000, 1);
-t=0:1:100;
-%axisLabels = axes;
-%axisLables.Title.string = 'binomial RV pmf';
-y = hist(x,t);
-title('binomial RV pmf');
-ylabel('number of samples');
-xlabel('number of positive outcomes');
 
-maxActualValue = find(y == max(y(:)))
-z = pdf(binomialDist,t);
-maxExpectedValue = find(z == max(z(:)))
+%Generate values based on binomial distribution
+x = random(binomialDist, 10000, 1);
+
+%plot values in a histogram
+histogram(x, 'Normalization', 'probability', 'BinWidth', 0.1);
+title('binomial RV pmf');
+ylabel('probability');
+xlabel('value of k');
+
+%compute the most occuring k value from the generated distribution
+[N,edges,bin] = histcounts(x,'Normalization', 'probability', 'BinWidth', 0.1);
+z = find(N == max(N(:)));
+fprintf('Maximum K value: %d\n', edges(z));
