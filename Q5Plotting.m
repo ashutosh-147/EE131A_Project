a302bbdb5c358aec0fb97e34c10b61acd748fd82
Ashutosh Shiledar <ashutosh@lnxsrv02.seas.ucla.edu>
2014-12-16 18:28:53 -0800

    added subscripts to Q5 plot titles and axis. Added comments and fixed code for Question 3.

--- Q5Plotting.m
+++ Q5Plotting.m
@@ -8,16 +8,16 @@ function [  ] = Q5Plotting( n, N, figNum )
     
     fig1 = figure(1+3*figNum);
     histogram(x, 'Normalization', 'pdf', 'BinWidth', 0.2);
-    title(strcat('S_n pdf - n = ',num2str(n)));
-    ylabel('Pdf values');
-    xlabel('Value of Random variable S_n');
+    title(strcat('S_{n} pdf - n = ',num2str(n)));
+    ylabel('f_{X}(x)');
+    xlabel('Value of Random variable S_{n}');
     saveas(fig1, strcat('Q5_figures/fig1_n_', num2str(n)), 'jpeg');
 
     fig2 = figure(2+3*figNum);
     histogram(x, 'Normalization', 'cdf', 'BinWidth', 0.2);
     title(strcat('S_n cdf - n = ',num2str(n)));
-    ylabel('Cdf values');
-    xlabel('Value of Random variable S_n');
+    ylabel('F_{X}(x)');
+    xlabel('Value of Random variable S_{n}');
     saveas(fig2, strcat('Q5_figures/fig2_n_', num2str(n)), 'jpeg');
     
     fig3 = figure(3+3*figNum);
@@ -25,9 +25,9 @@ function [  ] = Q5Plotting( n, N, figNum )
     hold on
     expected = makedist('Normal', 'mu', 3.5*n,'sigma',sqrt(n*25/12));
     plot(t, pdf(expected,t),'r');
-    title(strcat('Gaussian pdf overlayed with expected results - n = ', num2str(n)));
-    ylabel('Pdf values');
-    xlabel('Value of Random variable S_n');
+    title(strcat('Gaussian pdf superimposed on simulated results - n = ', num2str(n)));
+    ylabel('f_{X}(x)');
+    xlabel('Value of Random variable S_{n}');
     hold off
     saveas(fig3, strcat('Q5_figures/fig3_n_', num2str(n)), 'jpeg');
     
--- Question3.m
+++ Question3.m
@@ -1,26 +1,30 @@
 N = 10000;
+
+%generate distributions for 2 dice, them add up the results
 dice1 = ceil(6 * rand(N,1));
 dice2 = ceil(6 * rand(N,1));
 diceSum = dice1 + dice2;
 
+%calculate array P such that P(x) equals probability roll resulted in x
 P = zeros(12,1);
 for i = 1:12
     P(i) = sum(diceSum == i) / N;
 end
 
 % problem 3 a
-ProbWinningOnFirstRoll = P(7) + P(11)
+ProbWinningOnFirstRoll = P(7) + P(11);
+fprintf('Probability of winning on first roll: %f\n',ProbWinningOnFirstRoll);
 
 % problem 3 b
-% 2/5 chance of winning, 3/5 chance of losing
-ProbWinningHavingRolled5 = P(5) / (P(5) + P(7))
+ProbWinningHavingRolled5 = P(5) / (P(5) + P(7));
+fprintf('Probability of winning having first rolled a 5: %f\n',ProbWinningHavingRolled5);
 
 % problem 3 c
-ProbSecondRoll = P(4) + P(5) + P(6) + P(8) + P(9) + P(10);
 ProbWinSecondRoll(1) = P(4)^2 / (P(4) + P(7));
 ProbWinSecondRoll(2) = P(5)^2 / (P(5) + P(7));
 ProbWinSecondRoll(3) = P(6)^2 / (P(6) + P(7));
 ProbWinSecondRoll(4) = P(8)^2 / (P(8) + P(7));
 ProbWinSecondRoll(5) = P(9)^2 / (P(9) + P(7));
 ProbWinSecondRoll(6) = P(10)^2 / (P(10) + P(7));
-ProbWinning = ProbWinningOnFirstRoll + ProbSecondRoll * (sum(ProbWinSecondRoll))
+ProbWinning = ProbWinningOnFirstRoll + sum(ProbWinSecondRoll);
+fprintf('Probability of winning game: %f\n',ProbWinning);
