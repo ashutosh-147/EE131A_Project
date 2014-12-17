f63143ecc9fe82bab017e2aa13796b86029464f1
Ashutosh Shiledar <ashutosh@lnxsrv04.seas.ucla.edu>
2014-12-16 20:16:23 -0800

    Bin width now set to 1 in Question 1 for better graph. Question 5 does not sum for n = 1 to produce correct distribution. Thanks Prashanth Swaminathan for pointing both of these out!

--- Q5Plotting.m
+++ Q5Plotting.m
@@ -2,22 +2,25 @@ function [  ] = Q5Plotting( n, N, figNum )
 %Q5PLOTTING Summary of this function goes here
 %   Detailed explanation goes here
     
-    data = sum(1+5*rand(n,N));
-    x = data.';
+    if(n == 1)
+        x = 1+5*rand(N,1);
+    else
+        x = sum(1+5*rand(n,N)).';
+    end
     t = min(x):0.1:max(x);
     
     fig1 = figure(1+3*figNum);
     histogram(x, 'Normalization', 'pdf', 'BinWidth', 0.2);
     title(strcat('S_{n} pdf - n = ',num2str(n)));
     ylabel('f_{X}(x)');
-    xlabel('Value of Random variable S_{n}');
+    xlabel('Value of Random Variable S_{n}');
     saveas(fig1, strcat('Q5_figures/fig1_n_', num2str(n)), 'jpeg');
 
     fig2 = figure(2+3*figNum);
     histogram(x, 'Normalization', 'cdf', 'BinWidth', 0.2);
     title(strcat('S_n cdf - n = ',num2str(n)));
     ylabel('F_{X}(x)');
-    xlabel('Value of Random variable S_{n}');
+    xlabel('Value of Random Variable S_{n}');
     saveas(fig2, strcat('Q5_figures/fig2_n_', num2str(n)), 'jpeg');
     
     fig3 = figure(3+3*figNum);
@@ -27,7 +30,7 @@ function [  ] = Q5Plotting( n, N, figNum )
     plot(t, pdf(expected,t),'r');
     title(strcat('Gaussian pdf superimposed on simulated results - n = ', num2str(n)));
     ylabel('f_{X}(x)');
-    xlabel('Value of Random variable S_{n}');
+    xlabel('Value of Random Variable S_{n}');
     hold off
     saveas(fig3, strcat('Q5_figures/fig3_n_', num2str(n)), 'jpeg');
     
--- Question1.m
+++ Question1.m
@@ -5,12 +5,12 @@ binomialDist = makedist('Binomial','N',100,'p',0.2);
 x = random(binomialDist, 10000, 1);
 
 %plot values in a histogram
-histogram(x, 'Normalization', 'probability', 'BinWidth', 0.1);
+histogram(x, 'Normalization', 'probability');
 title('binomial RV pmf - N = 100 p = 0.2');
 ylabel('probability');
 xlabel('value of k');
 
 %compute the most occuring k value from the generated distribution
-[N,edges,bin] = histcounts(x,'Normalization', 'probability', 'BinWidth', 0.1);
+[N,edges,bin] = histcounts(x,'Normalization', 'probability');
 z = find(N == max(N(:)));
-fprintf('Maximum K value: %d\n', edges(z));
+fprintf('Maximum K value: %d\n', ceil(edges(z)));
