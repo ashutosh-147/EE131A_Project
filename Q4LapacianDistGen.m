b028e00ab0e09905d4453c063a3663e08e2b4b5f
Ashutosh Shiledar <ashutosh@lnxsrv02.seas.ucla.edu>
2014-12-16 16:44:56 -0800

    added comments to Question 4 code

--- Q4LapacianDistGen.m
+++ Q4LapacianDistGen.m
@@ -1,10 +1,18 @@
 function [ distribution ] = Q4LapacianDistGen( alpha, t )
-%Q4LAPACIANDISTGEN Summary of this function goes here
-%   Detailed explanation goes here
-
+%Q4LAPACIANDISTGEN generates a Laplacian distribution of size t based on the
+%specified alpha
+    
+    %generate uniform random values
     y = rand(t,1);
+    
+    %since this cdf only gives positive values, randomly assign values to
+    %negative
     signs = 2 * ((rand(t, 1) < 0.5) - 0.5);
+    
+    %apply inverse cdf to uniform random values
     distribution = -log(1-y)/alpha;
+    
+    %apply correct signs to the distribution
     distribution = distribution .* signs;
 end
 
--- Question4.m
+++ Question4.m
@@ -1,7 +1,9 @@
 N = 10000;
 
+%generate a Laplacian Distribution with alpha = 0.5
 dist0_5 = Q4LapacianDistGen(0.5, N);
 
+%plot the pdf
 fig1 = figure(1);
 histogram(dist0_5, 'Normalization', 'pdf', 'BinWidth', 0.2)
 title('Laplacian distribution pdf - alpha = 0.5');
@@ -9,6 +11,7 @@ ylabel('Pdf values');
 xlabel('Value of Random variable X');
 saveas(fig1, 'Q4_figures/pdf_alpha_0_5', 'jpeg');
 
+%plot the cdf
 fig2 = figure(2);
 histogram(dist0_5, 'Normalization', 'cdf', 'BinWidth', 0.2)
 title('Laplacian distribution cdf - alpha = 0.5');
@@ -16,8 +19,10 @@ ylabel('Cdf values');
 xlabel('Value of Random variable X');
 saveas(fig2, 'Q4_figures/cdf_alpha_0_5', 'jpeg');
 
+%generate a Laplacian Distribution with alpha = 2
 dist2 = Q4LapacianDistGen(2, N);
 
+%plot the pdf
 fig3 = figure(3);
 histogram(dist2, 'Normalization', 'pdf', 'BinWidth', 0.2)
 title('Laplacian distribution pdf - alpha = 2');
@@ -25,6 +30,7 @@ ylabel('Pdf values');
 xlabel('Value of Random variable X');
 saveas(fig3, 'Q4_figures/pdf_alpha_2', 'jpeg');
 
+%plot the cdf
 fig4 = figure(4);
 histogram(dist2, 'Normalization', 'cdf', 'BinWidth', 0.2)
 title('Laplacian distribution cdf - alpha = 2');
@@ -33,9 +39,20 @@ xlabel('Value of Random variable X');
 saveas(fig4, 'Q4_figures/cdf_alpha_2', 'jpeg');
 
 %snr 0 dB -> sigma = 1 -> alpha = sqrt(2)
-ones = Q4LapacianDistGen(sqrt(2), N/2) + 2;
-zeros = Q4LapacianDistGen(sqrt(2), N/2) - 2;
+
+%0's and 1's are equiprobable, so we need to randomly distribute them in
+%the generated sample
+binomialDist = makedist('Binomial','N',N,'p',0.5);
+numberOfOnes = random(binomialDist);
+
+%generate the full N samples from part samples of 0's and 1's
+ones = Q4LapacianDistGen(sqrt(2), numberOfOnes) + 2;
+zeros = Q4LapacianDistGen(sqrt(2), N-numberOfOnes) - 2;
 bits = cat(1, ones, zeros);
 
-e1 = sum(ones < 0) / (N/2)
-e0 = sum(zeros > 0) / (N/2)
+%calculate probability of the errors
+e1 = sum(ones < 0) / numberOfOnes;
+e0 = sum(zeros > 0) / (N-numberOfOnes);
+
+fprintf('Probability of false zero: %f\n', e1);
+fprintf('Probability of false one: %f\n', e0);
